71aa9541117a41c1444b3a3b7487e705952739fe
Fabian Ferno <fabianferno@gmail.com>
2020-12-06 23:19:06 +0530

    Update and smoothscroll js

--- droid/script.js
+++ droid/script.js
@@ -9,23 +9,23 @@ class App extends React.Component {
       mouseX: 0,
       toTheRight: true,
       speed: 2,
-      accelMod: 0.5 };
-
+      accelMod: 0.5,
+    };
   }
 
   // Keep track of the mouse position.
   handleMouseMove(event) {
     this.setState({
-      mouseX: event.pageX });
-
+      mouseX: event.pageX,
+    });
   }
 
   // Speed Mod Bar
   handleSpeedChange(e) {
     if (parseFloat(e.target.value)) {
       this.setState({
-        speed: e.target.value });
-
+        speed: e.target.value,
+      });
     }
   }
 
@@ -33,8 +33,8 @@ class App extends React.Component {
   handleAccelChange(e) {
     if (parseFloat(e.target.value)) {
       this.setState({
-        accelMod: e.target.value });
-
+        accelMod: e.target.value,
+      });
     }
   }
 
@@ -42,10 +42,9 @@ class App extends React.Component {
   movement() {
     let { droidX, mouseX, speed, accelMod } = this.state;
 
-    // Need a pretty strict if statement to make sure React doesn't end up in a 
+    // Need a pretty strict if statement to make sure React doesn't end up in a
     // render loop with all the state changes / re-rendering going on.
     if (Math.abs(Math.round(droidX) - mouseX) !== 1) {
-
       let distance = mouseX - droidX;
       let acceleration = Math.abs(distance * accelMod) / 100;
 
@@ -53,88 +52,115 @@ class App extends React.Component {
       if (droidX < mouseX) {
         this.setState({
           droidX: droidX + speed * acceleration,
-          toTheRight: true });
-
+          toTheRight: true,
+        });
       }
 
       // Move to the left
       else {
-          this.setState({
-            droidX: droidX - speed * acceleration,
-            toTheRight: false });
-
-        }
+        this.setState({
+          droidX: droidX - speed * acceleration,
+          toTheRight: false,
+        });
+      }
     }
   }
 
-  // Get some initial movement on first mount. 
+  // Get some initial movement on first mount.
   componentWillMount() {
     this.setState({
-      mouseX: 300 });
-
+      mouseX: 300,
+    });
   }
 
   // Set up the mouse event listener and fire up the movement function.
   componentDidMount() {
-    document.addEventListener('mousemove', e => this.handleMouseMove(e));
+    document.addEventListener("mousemove", (e) => this.handleMouseMove(e));
     setInterval(this.movement.bind(this), 1);
   }
 
   // Clean up.
   componentWillUnmount() {
-    document.removeEventListener('mousemove', e => this.handleMouseMove(e));
+    document.removeEventListener("mousemove", (e) => this.handleMouseMove(e));
   }
 
   // Away we go.
   render() {
     let { speed, accelMod, droidX, mouseX, toTheRight } = this.state;
 
-    return (
-      React.createElement("div", null,
-
-      React.createElement("div", { className: "logo" },
-      React.createElement("img", { src: "../logo_cross_black.png" })),
-
-      React.createElement("div", { className: "bb8", style: { WebkitTransform: `translateX(${droidX}px)` } },
-      React.createElement("div", { className: 'antennas ' + (toTheRight ? 'right' : ''),
-        style: { WebkitTransform: `translateX(${(mouseX - droidX) / 25}px) rotateZ(${(mouseX - droidX) / 80}deg)` } },
-      React.createElement("div", { className: "antenna short" }),
-      React.createElement("div", { className: "antenna long" })),
-
-      React.createElement("div", { className: "head",
-        style: { WebkitTransform: `translateX(${(mouseX - droidX) / 15}px) rotateZ(${(mouseX - droidX) / 25}deg)` } },
-      React.createElement("div", { className: "stripe one" }),
-      React.createElement("div", { className: "stripe two" }),
-
-      React.createElement("div", { className: 'stripe detail ' + (toTheRight ? 'right' : '') },
-      React.createElement("div", { className: "detail zero" }),
-      React.createElement("div", { className: "detail zero" }),
-      React.createElement("div", { className: "detail one" }),
-      React.createElement("div", { className: "detail two" }),
-      React.createElement("div", { className: "detail three" }),
-      React.createElement("div", { className: "detail four" }),
-      React.createElement("div", { className: "detail five" }),
-      React.createElement("div", { className: "detail five" })),
-
-      React.createElement("div", { className: "stripe three" })),
-
-      React.createElement("div", { className: "ball", style: { WebkitTransform: `rotateZ(${droidX / 2}deg)` } },
-      React.createElement("div", { className: "lines one" }),
-      React.createElement("div", { className: "lines two" }),
-      React.createElement("div", { className: "ring one" }),
-      React.createElement("div", { className: "ring two" }),
-      React.createElement("div", { className: "ring three" })),
-
-      React.createElement("div", { className: "shadow" })),
+    return React.createElement(
+      "div",
+      null,
 
 
-      React.createElement("div", { className: "instructions" },
-      React.createElement("p", null, "move your mouse."))));
+      React.createElement(
+        "div",
+        {
+          className: "bb8",
+          style: { WebkitTransform: `translateX(${droidX}px)` },
+        },
+        React.createElement(
+          "div",
+          {
+            className: "antennas " + (toTheRight ? "right" : ""),
+            style: {
+              WebkitTransform: `translateX(${
+                (mouseX - droidX) / 25
+              }px) rotateZ(${(mouseX - droidX) / 80}deg)`,
+            },
+          },
+          React.createElement("div", { className: "antenna short" }),
+          React.createElement("div", { className: "antenna long" })
+        ),
 
+        React.createElement(
+          "div",
+          {
+            className: "head",
+            style: {
+              WebkitTransform: `translateX(${
+                (mouseX - droidX) / 15
+              }px) rotateZ(${(mouseX - droidX) / 25}deg)`,
+            },
+          },
+          React.createElement("div", { className: "stripe one" }),
+          React.createElement("div", { className: "stripe two" }),
 
+          React.createElement(
+            "div",
+            { className: "stripe detail " + (toTheRight ? "right" : "") },
+            React.createElement("div", { className: "detail zero" }),
+            React.createElement("div", { className: "detail zero" }),
+            React.createElement("div", { className: "detail one" }),
+            React.createElement("div", { className: "detail two" }),
+            React.createElement("div", { className: "detail three" }),
+            React.createElement("div", { className: "detail four" }),
+            React.createElement("div", { className: "detail five" }),
+            React.createElement("div", { className: "detail five" })
+          ),
 
+          React.createElement("div", { className: "stripe three" })
+        ),
 
-  }}
+        React.createElement(
+          "div",
+          {
+            className: "ball",
+            style: { WebkitTransform: `rotateZ(${droidX / 2}deg)` },
+          },
+          React.createElement("div", { className: "lines one" }),
+          React.createElement("div", { className: "lines two" }),
+          React.createElement("div", { className: "ring one" }),
+          React.createElement("div", { className: "ring two" }),
+          React.createElement("div", { className: "ring three" })
+        ),
 
+        React.createElement("div", { className: "shadow" })
+      ),
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+     
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
---
+++ local_js/script.js
@@ -0,0 +1,48 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+owl.trigger('play.owl.autoplay',[2000]);
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scroll-loader").style.width = scrolled + "%";
+}
+
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $(".smooth").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
